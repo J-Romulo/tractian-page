a20997526c905701d3f8241bd684cadc4b545217
J-Romulo <joseromulo.10@hotmail.com>
2026-01-20 22:56:39 +1300

    chore: reorganise footer folder structure

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Linkedin, Facebook, Instagram, Youtube, Phone } from "lucide-react";
+import { Phone } from "lucide-react";
 import Logo from "../../icons/Logo";
 import { useLocale, useTranslations } from "next-intl";
 import BackToTopButton from "../../ui/BackToTopButton";
@@ -10,7 +10,7 @@ import {
   FooterSection,
   footerSections,
   footerSocialMedias,
-} from "./Footer.data";
+} from "./data";
 import { Chevron } from "../../icons/Chevron";
 
 function FooterLinkItem({
@@ -83,7 +83,7 @@ export default function Footer() {
   const locale = useLocale();
 
   return (
-    <footer className="bg-blue-950 px-4 py-12 lg:py-20 h-191">
+    <footer className="bg-blue-950 px-4 py-12 lg:py-20 h-full">
       <div className="mx-auto flex w-full max-w-6xl flex-col">
         <section className="mb-8 flex flex-col gap-y-8 lg:mb-12 lg:flex-row lg:items-center lg:justify-between">
           <div className="flex flex-wrap items-center justify-evenly gap-4 lg:justify-start lg:gap-x-6">
@@ -99,7 +99,7 @@ export default function Footer() {
           <BackToTopButton label={i18n("backToTop")} />
         </section>
 
-        <section className="mb-12 grid grid-cols-1 justify-between gap-y-3 sm:grid-cols-2 md:gap-x-6 md:gap-y-6 lg:flex lg:grid-cols-5 lg:justify-between lg:gap-x-8 lg:gap-y-12">
+        <section className="mb-12 text-[14px] lg:text-[16px] grid grid-cols-1 justify-between gap-y-3 sm:grid-cols-2 md:gap-x-6 md:gap-y-6 lg:flex lg:grid-cols-5 lg:justify-between lg:gap-x-8 lg:gap-y-12">
           {footerSections.map((section) => (
             <FooterSectionComponent
               key={section.titleKey}
@@ -123,17 +123,17 @@ export default function Footer() {
           </article>
         </section>
 
-        <div className="flex flex-col-reverse justify-between lg:flex-row">
+        <div className="flex flex-col-reverse justify-between gap-y-10 lg:flex-row">
           <div className="flex flex-col items-center gap-y-4 lg:items-start">
-            <Logo className="w-36 lg:w-40" fill="#FFFFFF" />
-            <p className="text-sm uppercase text-slate-50">
+            <Logo className="w-28 lg:w-40" fill="#FFFFFF" />
+            <p className="text-sm text-[12px] lg:text-[14px] uppercase text-slate-50">
               {i18n("copyright")}
               <br />
               {locale === "pt" && i18n("cnpj")}
             </p>
             <Link
               href="tel:08001102020"
-              className="flex items-center gap-x-2 text-sm text-slate-50 hover:underline"
+              className="flex items-center text-[14px] gap-x-2 text-sm text-slate-50 hover:underline"
             >
               <Phone className="h-5 w-5" /> {i18n("phone")}
             </Link>
